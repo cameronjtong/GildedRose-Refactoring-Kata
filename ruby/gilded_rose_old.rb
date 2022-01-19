bba956e39663e52605f8857f7ae635ad9c671fdd
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 16:47:28 -0500

    add characterization test

--- ruby/gilded_rose_old.rb
+++ ruby/gilded_rose_old.rb
@@ -1,4 +1,4 @@
-class GildedRose
+class GildedRoseOld
   def initialize(items)
     @items = items
   end
@@ -27,5 +27,19 @@ class GildedRose
         end
       end
     end
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s
+    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,33 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require_relative "gilded_rose"
+require_relative "gilded_rose_old"
 
 describe GildedRose do
-
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      items1 = build_items
+      items2 = build_items
+
+      100.times do
+        GildedRoseOld.new(items1).update_quality
+        GildedRose.new(items2).update_quality
+
+        items1.zip(items2).each do |item1, item2|
+          expect(item1).to have_attributes(
+            name: item2.name,
+            sell_in: item2.sell_in,
+            quality: item2.quality
+          )
+        end
+      end
     end
   end
 
+  def build_items
+    [
+      Item.new("Normal Item", 20, 40),
+      Item.new("Aged Brie", 20, 20),
+      Item.new("Backstage passes to a TAFKAL820ETC concert", 20, 20),
+      Item.new("Sulfuras, Hand of Ragnaros", 20, 20),
+    ]
+  end
 end

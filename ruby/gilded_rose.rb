edd35822b0503334ca54215af130d7a2b3d3979f
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 21:00:41 -0500

    apply flatten nested conditional again

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,21 +13,21 @@ class GildedRose
     case item.name
     when "Aged Brie"
       item.sell_in -= 1
-      item.quality += 1
-      item.quality += 1 if item.sell_in < 0
+      item.quality += item.sell_in < 0 ? 2 : 1
       item.quality = item.quality.clamp(0, 50)
     when "Backstage passes to a TAFKAL80ETC concert"
       item.sell_in -= 1
-      item.quality += 1
-      item.quality += 1 if item.sell_in < 10
-      item.quality += 1 if item.sell_in < 5
-      item.quality += 1 if item.sell_in < 0
+      case item.sell_in
+      when (-100..-1) then item.quality += 4
+      when (0..4) then item.quality += 3
+      when (5..9) then item.quality += 2
+      when (10..100) then item.quality += 1
+      end
       item.quality = item.quality.clamp(0, 50)
     when "Sulfuras, Hand of Ragnaros"
     else
       item.sell_in -= 1
-      item.quality -= 1
-      item.quality -= 1 if item.sell_in < 0
+      item.quality -= item.sell_in < 0 ? 2 : 1
       item.quality = item.quality.clamp(0, 50)
     end
   end

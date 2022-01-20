90458157d9d613d9ffb8c9169f8e5d0766d036b6
Cameron Tong <cameron.tong2b@gmail.com>
2022-01-19 20:44:44 -0500

    increase symmetry and refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -12,22 +12,23 @@ class GildedRose
   def update_item(item)
     case item.name
     when "Aged Brie"
-      item.quality = item.quality + 1 if item.quality < 50
-      item.sell_in = item.sell_in - 1
-      item.quality = item.quality + 1 if item.sell_in < 0 && (item.quality < 50)
+      item.sell_in -= 1
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 0
+      item.quality = item.quality.clamp(0, 50)
     when "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality < 50
-        item.quality = item.quality + 1
-        item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-        item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
-      end
-      item.sell_in = item.sell_in - 1
-      item.quality = item.quality + 1 if item.sell_in < 0 && (item.quality < 50)
+      item.sell_in -= 1
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 10
+      item.quality += 1 if item.sell_in < 5
+      item.quality += 1 if item.sell_in < 0
+      item.quality = item.quality.clamp(0, 50)
     when "Sulfuras, Hand of Ragnaros"
     else
-      item.quality = item.quality - 1 if item.quality > 0
-      item.sell_in = item.sell_in - 1
-      item.quality = item.quality - 1 if item.sell_in < 0 && item.quality > 0
+      item.sell_in -= 1
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in < 0
+      item.quality = item.quality.clamp(0, 50)
     end
   end
 end
